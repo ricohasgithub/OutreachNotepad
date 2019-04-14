88b096bcec8f17f0c21bf6b274087f8e7ae93d40
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-04-14 19:22:58 -0400

    Changed Import Structure
     - Moved firebase initialization to a seperate file named init-firebase.js

---
+++ src/js/init-firebase.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBs5DSqjer7HCmrgdY7DNkKMIFBD8DmEn4",
+  authDomain: "outreachlog-c8df3.firebaseapp.com",
+  databaseURL: "https://outreachlog-c8df3.firebaseio.com",
+  projectId: "outreachlog-c8df3",
+  storageBucket: "outreachlog-c8df3.appspot.com",
+  messagingSenderId: "686191595291"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
