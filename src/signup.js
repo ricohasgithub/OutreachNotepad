6eb56defe03aeaa29939f11bda5e3166d9776c8a
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-05-12 11:20:28 -0400

    Update index.html
     - Added header with intro text

---
+++ src/signup.js
@@ -0,0 +1,8 @@
+
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorCode);
+  console.log(errorMessage);
+});
