5a28e9300fc1ac6cf2e4f0bf951e72f1f00cb28a
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-05-19 10:56:38 -0400

    Made Info String Append Changes in Index.js
     - Instead of checking for DOM load, the main info string appender now checks for firebase user auth changes and whether it is valid or null

--- public/index.js
+++ public/index.js
@@ -1,16 +1,12 @@
 
-// User status variables
-const loggedin = false;
-
 // Html DOM elements
 
 // Main document body div
 let main = document.getElementById("main-body");
 
 // Event Listener for DOM intro text append
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("Method Called");
-  if (!loggedin) {
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user === null) {
     // If the has not loggedin yet, append message stating that the user must first sign in
     let logPrompt = document.createElement("H1");
     let logPromptText = document.createTextNode("Please Log in to Continue");
@@ -22,9 +18,19 @@ document.addEventListener('DOMContentLoaded', function() {
     logPrompt.style.align = 'center';
 
     main.appendChild(logPrompt);
-    console.log("Appended Child");
-  } else {
+  } else if (user !== null) {
     // User has signed in already, append with profile page and stats
+    // If the has not loggedin yet, append message stating that the user must first sign in
+    let logPrompt = document.createElement("H1");
+    let logPromptText = document.createTextNode("Hi");
 
+    logPrompt.appendChild(logPromptText);
+
+    // Add css
+    logPrompt.style.margin = '300px 435px';
+    logPrompt.style.align = 'center';
+
+    main.appendChild(logPrompt);
+    console.log("H");
   }
-}, false);
+});
