bf0e524b8995c85886489f40c20f5881fb2f8b0e
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-05-20 13:44:11 -0400

    Update index.html and index.js
     - Updated both index.html and index.js so that the login and signup a href header elements are all created upon discovering that there is no user currently logged in

--- public/index.js
+++ public/index.js
@@ -3,10 +3,35 @@
 
 // Main document body div
 let main = document.getElementById("main-body");
+// Sticky Header Menu div
+let header = document.getElementById("header-menu");
 
 // Event Listener for DOM intro text append
 firebase.auth().onAuthStateChanged(function(user) {
   if (user === null) {
+    // Sticky-Header text append with a href to login and signup pages
+    let loginHeader = document.createElement("a");
+    let signupHeader = document.createElement("a");
+
+    loginHeader.setAttribute("href", "login.html");
+    signupHeader.setAttribute("href", "signup.html");
+
+    loginHeader.setAttribute("id", "header-button");
+    signupHeader.setAttribute("id", "header-button");
+
+    let loginText = document.createTextNode("Log in");
+    let signupText = document.createTextNode("Sign up");
+
+    loginHeader.appendChild(loginText);
+    signupHeader.appendChild(signupText);
+
+    header.appendChild(loginHeader);
+    header.appendChild(signupHeader);
+
+    console.log("Got here");
+
+    // Main Body text append
+
     // If the has not loggedin yet, append message stating that the user must first sign in
     let logPrompt = document.createElement("H1");
     let logPromptText = document.createTextNode("Please Log in to Continue");
